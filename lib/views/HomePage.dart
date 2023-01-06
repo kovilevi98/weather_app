0afbc90fe8c60f9a7ed2751543d5464227af75f7
Levi <kovilevi98@gmail.com>
2023-01-06 16:07:09 +0100

    + add loading button

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:localization/localization.dart';
+import 'package:rounded_loading_button/rounded_loading_button.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -9,15 +13,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final RoundedLoadingButtonController _btnController = RoundedLoadingButtonController();
+
+  Future<void> _doSomething() async {
+    Timer(Duration(seconds: 3), () {
+      _btnController.stop();
+    });
+  }
+
+  Future<void> _reset() async {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _title(context),
+          _buildLocationButton(context)
+        ],
       ),
-      body: const Center(
-        child: Text("hallo"),
+    );
+  }
+
+  Widget _buildLocationButton(BuildContext context) {
+    return RoundedLoadingButton(
+      controller: _btnController,
+      onPressed: () async {
+        await _doSomething();
+      },
+      child: Text('refresh_weather'.i18n(), style: const TextStyle(color: Colors.white))
+    );
+  }
+
+  Widget _title(BuildContext context) {
+    return Expanded(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("hallo"),
+        ],
       ),
     );
   }
 }
+
